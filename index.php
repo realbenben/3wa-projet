2f00002f478e6da9610962ebae58ff93580609e9
realbenben <b.patou59@gmail.com>
2021-01-18 16:13:37 +0000

    architect fundation

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("./components/header.php"); ?>
+        <?php include("./components/footer.php"); ?> 
+</body>
+</html>
